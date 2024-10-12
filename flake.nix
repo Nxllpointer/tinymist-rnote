054986e598ee2b698734ee1491b69b2c4322d04c
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-10-12 18:38:47 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    typst.url = "github:Nxllpointer/typst?ref=rnote-tinymist-v0.11.33";
+  };
+
+  outputs = { typst, ... }: let
+    system = "x86_64-linux";
+    craneLib = typst.craneLib.${system};
+    dependencies = typst.dependencies.${system};
+
+    commonArgs = {
+      pname = "tinymist";
+      src = ./.;
+    } // dependencies;
+  in {
+
+    packages.${system} = rec {
+      tinymist = craneLib.buildPackage (commonArgs // {
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        doCheck = false;
+      });
+      default = tinymist;
+    };
+
+  };
+}
