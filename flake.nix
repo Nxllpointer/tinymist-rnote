baf434327d47bc67170d41ee3fe34632c3e17ebc
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-11-16 16:58:01 +0100

    Fix typst.ts submodule errors on recent nix versions

--- flake.nix
+++ flake.nix
@@ -5,24 +5,58 @@
     typst.url = "github:Nxllpointer/typst?ref=rnote";
   };
 
-  outputs = { typst, ... }: let
+  outputs = {typst, ...}: let
     system = "x86_64-linux";
     craneLib = typst.craneLib.${system};
     dependencies = typst.dependencies.${system};
 
-    commonArgs = {
-      pname = "tinymist";
-      src = ./.;
-    } // dependencies;
-  in {
+    parseSource = deps: let
+      sourceString = (builtins.elemAt deps 0).source;
+      url = builtins.elemAt (builtins.split "#" sourceString) 0;
+      parsed = builtins.parseFlakeRef url;
+    in
+      parsed;
 
-    packages.${system} = rec {
-      tinymist = craneLib.buildPackage (commonArgs // {
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        doCheck = false;
-      });
-      default = tinymist;
+    mkFetchGitOverrideAttrs = parsedSource: attrs: {
+      src = builtins.fetchGit (
+        {inherit (parsedSource) url rev;} // attrs
+      );
     };
 
+    isTypstTs = parsedSource: parsedSource.url == "https://github.com/Nxllpointer/typst.ts.git";
+
+    commonArgs =
+      {
+        pname = "tinymist";
+        src = ./.;
+      }
+      // dependencies;
+
+    cargoVendorDir = craneLib.vendorCargoDeps (commonArgs
+      // {
+        overrideVendorGitCheckout = deps: drv: let
+          parsedSource = parseSource deps;
+        in
+          # builtins.trace "Git checkout: ${builtins.toString (builtins.map (d: (builtins.map (n: "${n}: ${builtins.getAttr n d}") (builtins.attrNames d))) deps)}"
+          # builtins.trace "${source} | ${url} | ${ref.url}"
+          if isTypstTs parsedSource
+          then
+            builtins.trace "Patching typst.ts fetchGit!"
+            (drv.overrideAttrs (mkFetchGitOverrideAttrs parsedSource {submodules = false;}))
+          else drv;
+      });
+
+    cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {inherit cargoVendorDir;});
+
+    tinymist = craneLib.buildPackage (commonArgs
+      // {
+        inherit cargoArtifacts cargoVendorDir;
+        doCheck = false;
+      });
+  in {
+    packages.${system} = {
+      inherit tinymist;
+      default = tinymist;
+    };
   };
 }
